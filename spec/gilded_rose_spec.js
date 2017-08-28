88293219a9fc3845feae70f71fdba82e017a185a
Miguel Ángel Viera González <mangelviera93@gmail.com>
2017-08-28 19:40:58 +0200

    Update Jasmine and change kata structure

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -1,7 +1,15 @@
-describe("Gilded Rose", function() {
+describe("Gilded Rose", () => {
+  const items = []
+  
+  items.push(Item('+5 Dexterity Vest', 10, 20));
+  items.push(Item('Aged Brie', 2, 0));
+  items.push(Item('Elixir of the Mongoose', 5, 7));
+  items.push(Item('Sulfuras, Hand of Ragnaros', 0, 80));
+  items.push(Item('Backstage passes to a TAFKAL80ETC concert', 15, 20));
+  items.push(Item('Conjured Mana Cake', 3, 6));
 
-  it("should do something", function() {
-    update_quality();
+  it("should do something", () => {
+    update_quality(items);
   });
 
 });
