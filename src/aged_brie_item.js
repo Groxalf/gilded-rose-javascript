801de81e4fb9d766f09766d1753e302da30ea41c
Miguel Ángel Viera González <mangelviera93@gmail.com>
2017-08-31 01:21:12 +0200

    Finish Aged Brie Item refactor

--- src/aged_brie_item.js
+++ src/aged_brie_item.js
@@ -4,10 +4,10 @@ module.exports = function AgedBrieItem(name, sell_in, quality) {
     this.quality = quality;
 
     this.age = () => {
+        this.sell_in = this.sell_in - 1;
         if (this.quality < 50) {
             this.quality = this.quality + 1
         }
-        this.sell_in = this.sell_in - 1;
         if (this.sell_in < 0) {
             if (this.quality < 50) {
                 this.quality = this.quality + 1
