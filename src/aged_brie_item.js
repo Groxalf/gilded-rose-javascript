0bc8f19948e33460b576825b9d795679399a615c
Miguel Ángel Viera González <mangelviera93@gmail.com>
2017-08-31 01:17:49 +0200

    WIP Aged Brie Item

--- src/aged_brie_item.js
+++ src/aged_brie_item.js
@@ -9,20 +9,8 @@ module.exports = function AgedBrieItem(name, sell_in, quality) {
         }
         this.sell_in = this.sell_in - 1;
         if (this.sell_in < 0) {
-            if (this.name != 'Aged Brie') {
-                if (this.name != 'Backstage passes to a TAFKAL80ETC concert') {
-                    if (this.quality > 0) {
-                        if (this.name != 'Sulfuras, Hand of Ragnaros') {
-                            this.quality = this.quality - 1
-                        }
-                    }
-                } else {
-                    this.quality = this.quality - this.quality
-                }
-            } else {
-                if (this.quality < 50) {
-                    this.quality = this.quality + 1
-                }
+            if (this.quality < 50) {
+                this.quality = this.quality + 1
             }
         }
     }
