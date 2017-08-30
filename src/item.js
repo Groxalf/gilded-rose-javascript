979749f9ee39465b663e321060a785313b9543da
Miguel Ángel Viera González <mangelviera93@gmail.com>
2017-08-31 01:36:17 +0200

    WIP Regular item

--- src/item.js
+++ src/item.js
@@ -1,14 +1,11 @@
 const SulfurasItem = require('./sulfuras_item');
 const AgedBrieItem = require('./aged_brie_item');
 const BackstagePassItem = require('./backstage_pass_item');
+const RegularItem = require('./regular_item');
 
 module.exports = function Item(name, sell_in, quality) {
     if (name === 'Sulfuras Hand of Ragnaros') return new SulfurasItem(name, sell_in, quality);
     if (name === 'Aged Brie') return new AgedBrieItem(name, sell_in, quality);
     if (name === 'Backstage passes to a TAFKAL80ETC concert') return new BackstagePassItem(name, sell_in, quality);
-    return {
-      name: name,
-      sell_in: sell_in,
-      quality: quality
-    };
+    return new RegularItem(name, sell_in, quality);
   }
