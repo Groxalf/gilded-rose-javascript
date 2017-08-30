93cfab783e28018146cfc38e8ea71efffe331cde
Miguel Ángel Viera González <mangelviera93@gmail.com>
2017-08-31 01:16:31 +0200

    WIP Aged Brie Item

--- src/aged_brie_item.js
+++ src/aged_brie_item.js
@@ -6,37 +6,23 @@ module.exports = function AgedBrieItem(name, sell_in, quality) {
     this.age = () => {
         if (this.quality < 50) {
             this.quality = this.quality + 1
-            if (this.name == 'Backstage passes to a TAFKAL80ETC concert') {
-                if (this.sell_in < 11) {
-                    if (this.quality < 50) {
-                        this.quality = this.quality + 1
-                    }
-                }
-                if (this.sell_in < 6) {
-                    if (this.quality < 50) {
-                        this.quality = this.quality + 1
-                    }
-                }
-            }
         }
-    }
-    if (this.name != 'Sulfuras, Hand of Ragnaros') {
         this.sell_in = this.sell_in - 1;
-    }
-    if (this.sell_in < 0) {
-        if (this.name != 'Aged Brie') {
-            if (this.name != 'Backstage passes to a TAFKAL80ETC concert') {
-                if (this.quality > 0) {
-                    if (this.name != 'Sulfuras, Hand of Ragnaros') {
-                        this.quality = this.quality - 1
+        if (this.sell_in < 0) {
+            if (this.name != 'Aged Brie') {
+                if (this.name != 'Backstage passes to a TAFKAL80ETC concert') {
+                    if (this.quality > 0) {
+                        if (this.name != 'Sulfuras, Hand of Ragnaros') {
+                            this.quality = this.quality - 1
+                        }
                     }
+                } else {
+                    this.quality = this.quality - this.quality
                 }
             } else {
-                this.quality = this.quality - this.quality
-            }
-        } else {
-            if (this.quality < 50) {
-                this.quality = this.quality + 1
+                if (this.quality < 50) {
+                    this.quality = this.quality + 1
+                }
             }
         }
     }
