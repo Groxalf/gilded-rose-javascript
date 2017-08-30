fcfb96cac4c5bc453897c6ba9b72e1ee3d5c64db
Miguel Ángel Viera González <mangelviera93@gmail.com>
2017-08-31 01:44:49 +0200

    Refactor, use foreach instead of for loop

--- src/gilded_rose.js
+++ src/gilded_rose.js
@@ -1,7 +1,7 @@
 module.exports = {
   update_quality(items) {
-    for (var i = 0; i < items.length; i++) {
-        items[i].age();
-    }
+    items.forEach((item) => {
+      item.age();
+    });
   }
 }
